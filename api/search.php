8daa9d255e1f2446645ea51b3b099a44919e762b
Sastama <103018192+sastamaa@users.noreply.github.com>
2025-01-12 00:04:44 +0200

    Create search.php

---
+++ api/search.php
@@ -0,0 +1,24 @@
+<?php
+$query = strtolower($_GET['query'] ?? '');
+
+// Load data files
+$movies = json_decode(file_get_contents('../movies/list.json'), true);
+$series = json_decode(file_get_contents('../series/list.json'), true);
+
+// Merge and filter results
+$results = [];
+foreach ($movies as $movie) {
+    if (strpos(strtolower($movie['title'] ?? ''), $query) !== false) {
+        $results[] = $movie;
+    }
+}
+foreach ($series as $show) {
+    if (strpos(strtolower($show['title'] ?? ''), $query) !== false) {
+        $results[] = $show;
+    }
+}
+
+// Output JSON
+header('Content-Type: application/json');
+echo json_encode($results);
+?>
