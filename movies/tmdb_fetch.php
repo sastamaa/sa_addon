e7e340538de9308dfa8393ccfd40ff970da9e696
Sastama <103018192+sastamaa@users.noreply.github.com>
2025-01-12 01:24:47 +0200

    Update tmdb_fetch.php

--- movies/tmdb_fetch.php
+++ movies/tmdb_fetch.php
@@ -1,26 +1,46 @@
 <?php
-// Your TMDb API Key
-$apiKey = '28797e7035babad606ddbc1642d2ec8b';
+// TMDb API Key
+$api_key = "28797e7035babad606ddbc1642d2ec8b";
 
-// Get the TMDb ID from the request
-$tmdbId = $_GET['tmdb_id'] ?? null;
+// List of movies (add only tmdb_id and streaming_url here)
+$movies = [
+    [
+        "tmdb_id" => "585083", 
+        "streaming_url" => "https://s1.hdvbua.pro/media1/content/stream/films/hotel_transylvania_transformania_2022_webrip_1080p_83600/hls/1080/index.m3u8"
+    ],
+    [
+        "tmdb_id" => "616747", 
+        "streaming_url" => "https://s1.hdvbua.pro/media1/content/stream/films/haunted_mansion_2023_webdl_1080p_91673/hls/1080/index.m3u8"
+    ]
+];
 
-// If no TMDb ID is provided, return an error
-if (!$tmdbId) {
-    echo json_encode(['error' => 'Missing tmdb_id']);
-    exit;
-}
-
-// TMDb API URL for fetching movie details
-$tmdbApiUrl = "https://api.themoviedb.org/3/movie/$tmdbId?api_key=$apiKey&language=en-US";
+$output = [
+    "type" => "list",
+    "headline" => "Movies",
+    "entries" => []
+];
 
 // Fetch movie details from TMDb
-$response = file_get_contents($tmdbApiUrl);
-$movieDetails = json_decode($response, true);
+foreach ($movies as $movie) {
+    $tmdb_id = $movie['tmdb_id'];
+    $streaming_url = $movie['streaming_url'];
 
-// Add your streaming link to the movie data
-$movieDetails['streaming_url'] = $_GET['streaming_url'] ?? '';
+    // Fetch movie data from TMDb
+    $response = file_get_contents("https://api.themoviedb.org/3/movie/{$tmdb_id}?api_key={$api_key}&language=en-US");
+    $details = json_decode($response, true);
 
-// Return the data as JSON
+    // Check if the API call was successful
+    if (isset($details['title'])) {
+        $output['entries'][] = [
+            "title" => $details['title'],
+            "image" => "https://image.tmdb.org/t/p/w500" . $details['poster_path'],
+            "description" => $details['overview'],
+            "data" => $streaming_url
+        ];
+    }
+}
+
+// Output final JSON
 header('Content-Type: application/json');
-echo json_encode($movieDetails);
+echo json_encode($output, JSON_PRETTY_PRINT);
+?>
